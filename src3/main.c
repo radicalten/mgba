16eefe4c7fd55a36a593a1ebaf2f2bd3208beac6
radicalten <79894181+radicalten@users.noreply.github.com>
2024-07-13 11:20:08 -0500

    Update main.c

--- src3/main.c
+++ src3/main.c
@@ -12,21 +12,21 @@
 #include <unistd.h>
 #include <wiiuse/wpad.h>
 
-#include <include/common.h>
+#include <src3/include/common.h>
 
-#include <include/core.h>
+#include <src3/include/core.h>
 #include "feature/gui/gui-runner.h"
-#include <include/video.h>
-#include <include/audio.h>
-#include <include/gba.h>
-#include <include/input.h>
-#include <include/audio-resampler.h>
-#include <include/gui.h>
-#include <include/file-select.h>
-#include <include/font.h>
-#include <include/menu.h>
-#include <include/memory.h>
-#include <include/vfs.h>
+#include <src3/include/video.h>
+#include <src3/include/audio.h>
+#include <src3/include/gba.h>
+#include <src3/include/input.h>
+#include <src3/include/audio-resampler.h>
+#include <src3/include/gui.h>
+#include <src3/include/file-select.h>
+#include <src3/include/font.h>
+#include <src3/include/menu.h>
+#include <src3/include/memory.h>
+#include <src3/include/vfs.h>
 
 #ifdef WIIDRC
 #include <wiidrc/wiidrc.h>
