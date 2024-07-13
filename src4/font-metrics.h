24e5cf9211fd9aadd6d7234695886c5933ec02e9
radicalten <79894181+radicalten@users.noreply.github.com>
2024-07-13 11:51:31 -0500

    Update font-metrics.h

--- src4/font-metrics.h
+++ src4/font-metrics.h
@@ -6,8 +6,8 @@
 #ifndef DEFAULT_FONT_METRICS_H
 #define DEFAULT_FONT_METRICS_H
 
-#include <mgba-util/dllexports.h>
-#include <mgba-util/gui/font.h>
+#include "dllexports.h"
+#include "font.h"
 
 extern const struct GUIFontGlyphMetric defaultFontMetrics[128];
 extern const struct GUIIconMetric defaultIconMetrics[GUI_ICON_MAX];
