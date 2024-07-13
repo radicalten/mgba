2f202269b2067d07e4fc529f803ed9ee5247066d
radicalten <79894181+radicalten@users.noreply.github.com>
2024-07-13 11:15:22 -0500

    Update gui-font.c

--- src3/gui-font.c
+++ src3/gui-font.c
@@ -3,8 +3,8 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#include <mgba-util/gui/font.h>
-#include <mgba-util/gui/font-metrics.h>
+#include <include/font.h>
+#include <include/font-metrics.h>
 #include "icons.h"
 #include "font.h"
 
